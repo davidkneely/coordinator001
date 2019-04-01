e125bb074647dfd95398f8e1dc3c428d7fb666b4
davidkneely <davidics613@gmail.com>
2019-04-01 12:14:49 -1000

    Finishes code for the MainCoordinator

---
+++ coordinator001/MainCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  MainCoordinator.swift
+//  coordinator001
+//
+//  Created by David on 4/1/19.
+//  Copyright © 2019 Vision Runner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainController: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController.instantiate()
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
