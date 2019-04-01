d46d616616967a007dbfad307e552f69bedc27a1
davidkneely <davidics613@gmail.com>
2019-04-01 12:05:03 -1000

    Initial Commit

---
+++ coordinator001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coordinator001
+//
+//  Created by David on 4/1/19.
+//  Copyright © 2019 Vision Runner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
